9d7f7db90cd66b1f346cbf20729f255d6040c846
kimseonmi <zlatjs2@gmail.com>
2017-12-19 20:14:23 +0900

    todo 준비

--- filterList/src/App.js
+++ filterList/src/App.js
@@ -1,40 +1,12 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-// import * as actions from './actions';
-import Counter from './Counter';
 import Filter from './Filter';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-
-    };
-    this.onCreate = this.onCreate.bind(this);
-    this.onRemove = this.onRemove.bind(this);
-  }
-
-  onCreate() {
-    console.log('추가');
-    
-  }
-  
-  onRemove() {
-    console.log('삭제');
-  }
-
   render() {
     
     return (
       <div>
-        <Counter/>
-        <div>
-          <button onClick={this.onCreate}>추가</button>
-          <button onClick={this.onRemove}>삭제</button>
-        </div>
-        <br/><br/><br/>
         <Filter/>
-        <br/><br/><br/>
       </div>
     );
   }
