08b8a2e2e7a991470c5621a636a3b79b04ad9c17
kimseonmi <zlatjs2@gmail.com>
2017-12-19 20:10:32 +0900

    테스트 파일 제거

--- counter/src/index.js
+++ counter/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+import App from './App';
+
 import './index.css';
 
-import { createStore } from 'redux';
-import reducers from './modules';
-import { Provider } from 'react-redux';
-
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
